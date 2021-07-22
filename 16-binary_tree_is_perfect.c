ef3b3f614060a181aebe2288ae65ce08b8b4e362
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-07-22 09:14:47 +0300

    Update 16-binary_tree_is_perfect.c

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,12 +7,10 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int l, r;
-
+	
 	if (!tree)
 		return (0);
-	l = binary_tree_is_full(tree->left);
-	r = binary_tree_is_full(tree->right);
+	
 	return (l & r & (binary_tree_balance(tree) == 0));
 }
 
