d60b6c5158e99c9658a3b50023c34f3fe3534b73
Candiepih <44834632+candiepih@users.noreply.github.com>
2021-07-22 08:55:33 +0300

    Update 13-binary_tree_nodes.c

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,6 +1,20 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - checks if a certain node is a leaf node
+ * @node: node to check
+ * Return: nothing
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (!node)
+		return (0);
+	if (!node->left && !node->right && node)
+		return (1);
+	return (0);
+}
+
+/**
  * binary_tree_nodes - counts the number with at
  * least 1 child in a binary tree
  *
